3466a0425adcafc4ab5aae0c7146b0835bd9c6a9
Atsushi Sasaki <atsaki01@gmail.com>
2014-11-24 10:30:39 +0900

    fix util_test.go

--- provider/util_test.go
+++ provider/util_test.go
@@ -1,28 +1,20 @@
 package cloudstack
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/atsaki/golang-cloudstack-library"
+)
 
 type Object struct {
-	Name string
-	Id   string
+	Name cloudstack.NullString
+	Id   cloudstack.ID
 }
 
 func TestEqualName(t *testing.T) {
 	name := "objectname"
-	obj := Object{Name: name}
-	if !equalName(obj, name) {
-		t.Errorf("equalName failed. return false, expected true.")
-	}
-	if equalName(obj, "abracadabra") {
-		t.Errorf("equalName failed. return trule, expected false.")
-	}
-}
-
-func TestNameToId(t *testing.T) {
-	name := "objectname"
-	obj := Object{Name: name, Id: "1"}
-	nameToID()
-
+	obj := Object{}
+	obj.Name.Set(name)
 	if !equalName(obj, name) {
 		t.Errorf("equalName failed. return false, expected true.")
 	}

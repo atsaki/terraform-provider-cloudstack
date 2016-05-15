218c9866ac544bb857e5d8696c1cc2a235aae51f
Atsushi Sasaki <atsaki01@gmail.com>
2016-05-16 01:41:16 +0900

    fix examples

--- examples/terraform.tf
+++ examples/terraform.tf
@@ -1,16 +1,16 @@
-variable "endpoint" {}
+variable "end_point" {}
 variable "api_key" {}
 variable "secret_key" {}
 
 provider "cs" {
-  endpoint   = "${var.endpoint}"
+  end_point   = "${var.end_point}"
   api_key    = "${var.api_key}"
   secret_key = "${var.secret_key}"
 }
 
-resource "cs_virtualmachine" "vm01" {
+resource "cs_virtual_machine" "vm01" {
   zone_name = "zone01"
-  serviceoffering_name = "t1.micro"
+  service_offering_name = "t1.micro"
   template_name = "CentOS6.5"
   display_name = "vm01"
 }
